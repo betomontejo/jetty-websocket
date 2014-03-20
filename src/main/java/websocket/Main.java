2be44b35f80243d65374f7e690283938407fdafe
Beto Montejo <cmontejo@gmail.com>
2014-03-20 22:36:50 +0000

    Add configuration and main class for server startup.

---
+++ src/main/java/websocket/Main.java
@@ -0,0 +1,12 @@
+package websocket;
+
+import org.eclipse.jetty.server.Server;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.start();
+        server.join();
+    }
+}
